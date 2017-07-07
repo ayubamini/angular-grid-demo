f0769f83d0205f24e055c383f3d0d576373ac39c
Aioub <ayyub.amini@gmail.com>
2017-07-08 00:14:42 +0430

    add a simple smart table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 import { AppComponent } from './app.component';
 import { SmartTableComponent } from './smart-table/smart-table.component';
@@ -17,6 +18,7 @@ import { AngularDataTableComponent } from './angular-datatable/angular-datatable
   ],
   imports: [
     BrowserModule,
+    Ng2SmartTableModule,
     RouterModule.forRoot([{ path: 'smarttable', component: SmartTableComponent },
     { path: 'ngx-dt', component: NgxDataTableComponent },
     { path: 'angular-dt', component: AngularDataTableComponent }]),
--- src/app/smart-table/smart-table.component.ts
+++ src/app/smart-table/smart-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+
+
 
 @Component({
     selector: 'app-smart-t',
@@ -6,7 +8,48 @@ import { Component } from '@angular/core'
 })
 
 
-export class SmartTableComponent{
+export class SmartTableComponent {
     title = 'Smart Table Demo';
+
+    settings = {
+        columns: {
+            id: {
+                title: 'ID'
+            },
+            name: {
+                title: 'Full Name'
+            },
+            username: {
+                title: 'User Name'
+            },
+            email: {
+                title: 'Email'
+            }
+        }
+    };
+
+    data = [
+        {
+            id: 1,
+            name: "Leanne Graham",
+            username: "Bret",
+            email: "Sincere@april.biz"
+        },
+        {
+            id: 2,
+            name: "Ervin Howell",
+            username: "Antonette",
+            email: "Shanna@melissa.tv"
+        },
+
+        // ... list of items
+
+        {
+            id: 11,
+            name: "Nicholas DuBuque",
+            username: "Nicholas.Stanton",
+            email: "Rey.Padberg@rosamond.biz"
+        }
+    ];
 }
 
