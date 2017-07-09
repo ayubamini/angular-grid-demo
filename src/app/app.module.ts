f7075910c6ea3086dc6b070fbe139b22c23f5c08
Aioub <ayyub.amini@gmail.com>
2017-07-09 23:24:10 +0430

    add paging, sorting and RTL provider to kendo-ui grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { RouterModule, Routes } from '@angular/router';
+
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { RTL } from '@progress/kendo-angular-l10n';
+
 
 import { AppComponent } from './app.component';
 import { SmartTableComponent } from './smart-table/smart-table.component';
 import { NgxDataTableComponent } from './ngx-datatable/ngx-datatable.component';
 import { AngularDataTableComponent } from './angular-datatable/angular-datatable.component';
+import { KendouiComponent } from './kendoui-datatable/kendoui.component'
 
 
 @NgModule({
@@ -15,17 +24,26 @@ import { AngularDataTableComponent } from './angular-datatable/angular-datatable
     AppComponent,
     SmartTableComponent,
     NgxDataTableComponent,
-    AngularDataTableComponent
+    AngularDataTableComponent,
+    KendouiComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    
     Ng2SmartTableModule,
     NgxDatatableModule,
-    RouterModule.forRoot([{ path: 'smarttable', component: SmartTableComponent },
-    { path: 'ngx-dt', component: NgxDataTableComponent },
-    { path: 'angular-dt', component: AngularDataTableComponent }]),
+    ButtonsModule,
+    GridModule,
+
+    RouterModule.forRoot([
+      { path: 'smarttable', component: SmartTableComponent },
+      { path: 'ngx-dt', component: NgxDataTableComponent },
+      { path: 'angular-dt', component: AngularDataTableComponent },
+      { path: 'kendoui-dt', component: KendouiComponent }]),
   ],
-  providers: [],
+  providers: [{ provide: RTL, useValue: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/kendoui-datatable/kendoui.component.ts
+++ src/app/kendoui-datatable/kendoui.component.ts
@@ -1,19 +1,51 @@
 import { Component } from '@angular/core';
 import { products } from '../../assets/data/products';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query';
+
+
 
 @Component({
     selector: 'app-kendoui-dt',
     templateUrl: `./kendoui.component.html`,
-    styleUrls: ['../../styles.scss'],
 })
 export class KendouiComponent {
     title: string = 'Kendo-UI!';
-    public value: string = "359884123321";
-    public mask: string = "(999) 000-00-00-00";
 
-    private gridData: any[] = products;
+    /* Sorting */
+    public multiple: boolean = false;
+    public allowUnsort: boolean = true;
+    private sort: SortDescriptor[] = [];
 
-    onButtonClick() {
-        this.title = 'Hello from Kendo UI!';
+
+    /* Paging */
+    private data: Object[];
+    private pageSize: number = 10;
+    private skip: number = 0;
+
+    /* Data */
+    private gridView: GridDataResult;
+    private items: any[] = products;
+
+    constructor() {
+        this.loadItems();
     }
+
+    protected sortChange(sort: SortDescriptor[]): void {
+        this.sort = sort;
+        this.loadItems();
+    }
+
+    protected pageChange(event: PageChangeEvent): void {
+        this.skip = event.skip;
+        this.loadItems();
+    }
+
+    private loadItems(): void {
+        this.gridView = {
+            data: orderBy(this.items.slice(this.skip, this.skip + this.pageSize), this.sort),
+            total: this.items.length
+        };
+    }
+
 }
